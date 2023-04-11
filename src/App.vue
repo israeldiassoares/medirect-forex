31a285119e914112666c13ff7bd5c3f1556fd64f
Israel Soares <israeldiassoares@gmail.com>
2023-04-12 00:04:39 +0200

    setup project|Tailwind|Vue3TS|Vite|e2e

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { defineAsyncComponent } from "vue"
+const Header = defineAsyncComponent({
+  loader: () => import('@/components/organisms/OrganismHeader.vue'),
+
+})
+</script>
+
+<template>
+  <Header />
+  <main>
+    <RouterView />
+  </main>
+</template>
