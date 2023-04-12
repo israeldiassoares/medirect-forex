8289f71e226b777b5b3b76cebfaa0dbb7289a098
Israel Soares <israeldiassoares@gmail.com>
2023-04-13 00:54:49 +0200

    [feat]created LineChart|DemoValues

---
+++ src/components/atoms/AtomLineChart.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div style="width: 800px;">
+            <canvas id="acquisitions"></canvas>
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Acquisitions } from '@/Acquisitions'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    Acquisitions()
+})
+</script>
