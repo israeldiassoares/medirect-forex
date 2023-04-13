c8c0329ac303578921e87f472823967ca837b683
Israel Soares <israeldiassoares@gmail.com>
2023-04-13 22:47:50 +0200

    feat]HandleExchangePrice|PlotChart|TreatDataByTime

--- src/components/organisms/OrganismHome.vue
+++ src/components/organisms/OrganismHome.vue
@@ -1,12 +1,33 @@
 <template>
     <div class="grid grid-rows-2 ">
+
         <section id="available-exchange">
             <DropDownSelect />
         </section>
         <section id="compare-exchange">
-            <DropDownSelect @change="store.setSelectedBaseCurrency($event.target.value)" />
-            <DropDownSelect @change="[ store.setSelectedComparedCurrency($event.target.value), fetchLiveCurrency() ]" />
-            <!-- <AtomLineChart /> -->
+            <DropDownSelect @change="store.setSelectedBaseCurrency($event.target.value)">
+                <template v-slot:flag>
+                    <div
+                        class="ml-4 currency-flag"
+                        :class="`currency-flag-${store.getSelectedBaseCurrency.toLocaleLowerCase()}`"
+                    >
+                    </div>
+                </template>
+
+            </DropDownSelect>
+            <DropDownSelect @change="[ store.setSelectedQuoteCurrency($event.target.value), store.fetchLiveCurrency() ]">
+                <template v-slot:flag>
+
+                    <div
+                        class="ml-4 currency-flag"
+                        :class="`currency-flag-${store.getSelectedQuoteCurrency.toLocaleLowerCase()}`"
+                    >
+                    </div>
+                </template>
+            </DropDownSelect>
+        </section>
+        <section id="chart">
+            <AtomLineChart />
         </section>
     </div>
 </template>
@@ -14,19 +35,16 @@
 <script setup lang="ts">
 import { defineAsyncComponent, onMounted } from "vue"
 import { useLiveCurrency } from '@/stores/liveCurrency'
+const store = useLiveCurrency()
 const DropDownSelect = defineAsyncComponent({
     loader: () => import('@/components/atoms/AtomDropDown.vue')
 })
-// const AtomLineChart = defineAsyncComponent({
-//     loader: () => import('@/components/atoms/AtomLineChart.vue')
-// })
-const store = useLiveCurrency()
+const AtomLineChart = defineAsyncComponent({
+    loader: () => import('@/components/atoms/AtomLineChart.vue')
+})
+
 onMounted(() => {
     store.fetchListCurrency()
 })
-function fetchLiveCurrency() {
-    store.fetchLiveCurrency()
-}
-
 
 </script>
